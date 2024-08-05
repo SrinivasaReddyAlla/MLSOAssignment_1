c8898c979da7f4a9a521d3ddf2ab8fdbb8b59324
SrinivasaReddyAlla <2022ac05322@wilp.bits-pilani.ac.in>
2024-08-05 04:36:16 +0200

    req updated

--- Src/train.py
+++ Src/train.py
@@ -6,7 +6,6 @@ tf.random.set_seed(42)
 (train_images, train_labels), _ = datasets.mnist.load_data()
 train_images = train_images / 255.0
 train_images = np.expand_dims(train_images, axis=-1)        
-
 model = models.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
     layers.MaxPooling2D((2, 2)),
@@ -14,9 +13,8 @@ model = models.Sequential([
     layers.Dense(64, activation='relu'),
     layers.Dense(10)
 ])        
-
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 model.fit(train_images, train_labels, epochs=5)
-model.save("mnist_model.h5") 
+model.save("mnist_model.h5")
